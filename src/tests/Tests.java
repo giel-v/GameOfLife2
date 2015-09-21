0353f60be8b7b304d166a088f083173bceecc7a1
giel-v <gielvanhoudt@hotmail.com>
2015-09-21 12:15:46 +0200

    init

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -2,13 +2,16 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Tests {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		World world = new World();
+		Cell cell = new Cell();
+		Assert.assertNotNull(world.getNeighbours(cell));
 	}
 
 }
---
+++ src/tests/World.java
@@ -0,0 +1,12 @@
+package tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class World {
+
+	public List getNeighbours(Cell cell) {
+		return new ArrayList<Cell>();
+	}
+
+}
